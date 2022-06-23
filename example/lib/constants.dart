da429798d1f0324cae22b55272d0a2e93f711fce
Rupinderjeet Singh Hans <rupinderjeet47@gmail.com>
2022-06-23 13:22:51 +0530

    fixes outdated video-links for video-resolution example

--- example/lib/constants.dart
+++ example/lib/constants.dart
@@ -14,13 +14,13 @@ class Constants {
       "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8";
   static const Map<String, String> exampleResolutionsUrls = {
     "LOW":
-        "https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4",
+        "https://file-examples.com/storage/fee788409562ada83b58ed5/2017/04/file_example_MP4_480_1_5MG.mp4",
     "MEDIUM":
-        "https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_640_3MG.mp4",
+        "https://file-examples.com/storage/fee788409562ada83b58ed5/2017/04/file_example_MP4_640_3MG.mp4",
     "LARGE":
-        "https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_1280_10MG.mp4",
+        "https://file-examples.com/storage/fee788409562ada83b58ed5/2017/04/file_example_MP4_1280_10MG.mp4",
     "EXTRA_LARGE":
-        "https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_1920_18MG.mp4"
+        "https://file-examples.com/storage/fee788409562ada83b58ed5/2017/04/file_example_MP4_1920_18MG.mp4"
   };
   static const String phantomVideoUrl =
       "http://sample.vodobox.com/skate_phantom_flex_4k/skate_phantom_flex_4k.m3u8";
